cfe0694f86f73b4d52f6fc6be35ca62ee474b6a3
Garrett <garrettpizzacalla@gmail.com>
2015-06-16 07:56:47 -0400

    Version update

--- src/main/java/ict/ICT.java
+++ src/main/java/ict/ICT.java
@@ -25,7 +25,7 @@ public class ICT
 {
     public static final String MOD_ID = "ict";
     public static final String MOD_NAME = "Interactive Crafting Table";
-    public static final String VERSION = "1.7.10-1.1.1";
+    public static final String VERSION = "1.7.10-1.2";
     public static final String CONFIG_NAME = MOD_NAME;
     public static final String CLIENT_PROXY_CLASS = "ict.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "ict.proxy.ServerProxy";
