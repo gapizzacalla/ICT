f4ddc89aa96896702ffc2f1f334218411b240203
Garrett <garrettpizzacalla@gmail.com>
2015-06-30 18:53:45 -0400

    Commit

--- src/main/java/ict/client/renderer/ItemRendererICT.java
+++ src/main/java/ict/client/renderer/ItemRendererICT.java
@@ -1,12 +1,7 @@
 package ict.client.renderer;
 
-import ict.model.ModelICT;
 import ict.tileentity.TileEntityICT;
-import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.tileentity.TileEntityBeaconRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.IItemRenderer;
 
