a14ea14af8851564cf3cb56e221e790909720e9f
Garrett <garrettpizzacalla@gmail.com>
2015-06-30 20:36:05 -0400

    Small changes

--- src/main/java/ict/helper/RenderHelper.java
+++ src/main/java/ict/helper/RenderHelper.java
@@ -4,18 +4,28 @@ import ict.ICT;
 import ict.tileentity.TileEntityICT;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.item.EntityItemFrame;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 import net.minecraftforge.client.IItemRenderer;
+import org.lwjgl.opengl.GL11;
 
 public class RenderHelper implements IItemRenderer
 {
@@ -34,9 +44,16 @@ public class RenderHelper implements IItemRenderer
     @Override
     public void renderItem(ItemRenderType type, ItemStack itemStack, Object... data)
     {
-        if (itemStack.getDisplayName().equals(new ItemStack(ICT.ict, 1).getDisplayName()))
+        if (type == ItemRenderType.EQUIPPED || type == ItemRenderType.EQUIPPED_FIRST_PERSON || type == ItemRenderType.INVENTORY)
         {
-            TileEntityRendererDispatcher.instance.renderTileEntityAt(new TileEntityICT(), 0.0D, 0.0D, 0.0D, 0.0F);
+            if (itemStack.getDisplayName().equals(new ItemStack(ICT.ict, 1).getDisplayName()))
+            {
+                TileEntityRendererDispatcher.instance.renderTileEntityAt(new TileEntityICT(), 0.0D, 0.0D, 0.0D, 1.0F);
+            }
+        }
+        else if (type == ItemRenderType.ENTITY)
+        {
+
         }
     }
 
@@ -46,8 +63,8 @@ public class RenderHelper implements IItemRenderer
         {
             if (itemStack.getItem() instanceof ItemBlock)
             {
-                Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
-                drawTextureFromIcon(index * 18, offset * 18, itemStack.getIconIndex(), 16, 16);
+                //Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
+                //drawTextureFromIcon(index * 18, offset * 18, itemStack.getIconIndex(), 16, 16);
             }
             else
             {
@@ -62,10 +79,10 @@ public class RenderHelper implements IItemRenderer
     {
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
-        tessellator.addVertexWithUV((double)(p_94065_1_ + 0), (double)(p_94065_2_ + p_94065_5_), 0, (double)p_94065_3_.getMinU(), (double)p_94065_3_.getMaxV());
+        tessellator.addVertexWithUV((double)(p_94065_1_), (double)(p_94065_2_ + p_94065_5_), 0, (double)p_94065_3_.getMinU(), (double)p_94065_3_.getMaxV());
         tessellator.addVertexWithUV((double)(p_94065_1_ + p_94065_4_), (double)(p_94065_2_ + p_94065_5_), 0, (double)p_94065_3_.getMaxU(), (double)p_94065_3_.getMaxV());
-        tessellator.addVertexWithUV((double)(p_94065_1_ + p_94065_4_), (double)(p_94065_2_ + 0), 0, (double)p_94065_3_.getMaxU(), (double)p_94065_3_.getMinV());
-        tessellator.addVertexWithUV((double)(p_94065_1_ + 0), (double)(p_94065_2_ + 0), 0, (double)p_94065_3_.getMinU(), (double)p_94065_3_.getMinV());
+        tessellator.addVertexWithUV((double)(p_94065_1_ + p_94065_4_), (double)(p_94065_2_), 0, (double)p_94065_3_.getMaxU(), (double)p_94065_3_.getMinV());
+        tessellator.addVertexWithUV((double)(p_94065_1_), (double)(p_94065_2_), 0, (double)p_94065_3_.getMinU(), (double)p_94065_3_.getMinV());
         tessellator.draw();
     }
 }
